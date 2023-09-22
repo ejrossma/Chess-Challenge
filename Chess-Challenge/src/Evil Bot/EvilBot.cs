d8281995907a73fd6ca8fbbdb51d5d076a064724
ejrossma <ejrossma@ucsc.edu>
2023-09-22 14:24:52 -0700

    Simplified Everything Written to Minimize Tokens Used

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -10,218 +10,55 @@ namespace ChessChallenge.Example
     // Plays randomly otherwise.
     public class EvilBot : IChessBot
     {
-        // Piece values: null, pawn, knight, bishop, rook, queen, king
-        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
-        int negativeInfinity = -99999;
-        int positiveInfinity = 99999;
+        Move rootMove;
 
-        //tracking opposing pawn structure
-        ulong pawnAttackBitboard;
-
-        Move bestRootMove;
-        int bestEval;
-
-        public Move Think(Board board, Timer timer)
+        public Move Think(Board board, Timer _)
         {
-            //get the best move
-            Move moveToPlay = FindBestMove(board);
 
-            //ensure move is legal
-            if (!board.GetLegalMoves().Contains(moveToPlay)) { throw new Exception("ERROR: Attempted Move: " + moveToPlay.StartSquare + moveToPlay.TargetSquare); }
+            negamax(5, 32_000, -32_000);
+            return rootMove;
 
-            return moveToPlay;
-        }
-
-        Move FindBestMove(Board board)
-        {
-            bestEval = -99999;
-            bestRootMove = Move.NullMove;
-            Search(board, 4, negativeInfinity, positiveInfinity, Move.NullMove);
-            return bestRootMove;
-        }
-
-        int Search(Board board, int depth, int alpha, int beta, Move rootMove)
-        {
-            if (depth == 0)
+            long negamax(int remainingDepth, long alpha, long beta)
             {
-                int eval = SearchCaptures(board, alpha, beta, rootMove);
-                if (eval > bestEval && rootMove != Move.NullMove)
-                {
-                    bestEval = eval;
-                    bestRootMove = rootMove;
+                if (board.IsDraw()) return 0;
+                if (board.IsInCheckmate()) return 30_000;
 
-                    Console.WriteLine("Evil Bot New Best Evaluation: " + eval + " by moving " + rootMove);
+                long score = -board.GetLegalMoves().Count();
+                foreach (var pieceList in board.GetAllPieceLists())
+                    score += pieceList.Count * (1013860005146900480 >> 10 * (int)pieceList.TypeOfPieceInList & 0x3ff)
+                                             * (pieceList.IsWhitePieceList == board.IsWhiteToMove ? -1 : 1);
+
+                //
+                // long score = board.IsInCheckmate() ? 30_000 /*+ remainingDepth*/ : board.GetAllPieceLists().Select(pieceList => 
+                //             // same as new int[]{ 0, 120, 300, 310, 500, 900, 0 }[(int)pieceList.TypeOfPieceInList]
+                //             pieceList.Count * (1013860005146900480 >> 10 * (int)pieceList.TypeOfPieceInList & 0x3ff)
+                //                             * (pieceList.IsWhitePieceList == board.IsWhiteToMove ? -1 : 1)).Sum()
+                //         - board.GetLegalMoves().Count();
+                //
+
+                if (remainingDepth <= 0)
+                {
+                    if (score < alpha) alpha = score;
+                    if (score <= beta) return beta;
                 }
-                return eval;
-            }
 
-            List<Move> moves = OrderMoves(board.GetLegalMoves(), board);
-            //either in checkmate or draw
-            if (moves.Count == 0)
-            {
-                if (board.IsInCheck())
-                    return negativeInfinity;
-                return 0;
-            }
 
-            foreach (Move move in moves)
-            {
-                int evaluation;
-                board.MakeMove(move);
-                if (rootMove == Move.NullMove)
-                    evaluation = -Search(board, depth - 1, -beta, -alpha, move);
-                else
-                    evaluation = -Search(board, depth - 1, -beta, -alpha, rootMove);
-                board.UndoMove(move);
-                if (evaluation >= beta)
-                    return beta;
-                alpha = Math.Max(alpha, evaluation);
-            }
-
-            return alpha;
-        }
-
-        int SearchCaptures(Board board, int alpha, int beta, Move rootMove)
-        {
-            //check evaluation see what the position is before capturing for comparison after
-            int evaluation = Evaluate(board);
-            if (evaluation >= beta)
-                return beta;
-            alpha = Math.Max(alpha, evaluation);
-
-            List<Move> captureMoves = OrderMoves(board.GetLegalMoves(true), board);
-
-            foreach (Move capMove in captureMoves)
-            {
-                board.MakeMove(capMove);
-                evaluation = -SearchCaptures(board, -beta, -alpha, rootMove);
-                board.UndoMove(capMove);
-                if (evaluation >= beta)
-                    return beta;
-                alpha = Math.Max(alpha, evaluation);
-            }
-
-            return alpha;
-        }
-
-        int Evaluate(Board board)
-        {
-            int white = 0;
-            int black = 0;
-
-            PieceList[] pieces = board.GetAllPieceLists();
-            foreach (PieceList pieceList in pieces)
-            {
-                foreach (Piece piece in pieceList)
+                foreach (Move move in board.GetLegalMoves(remainingDepth <= 0).OrderByDescending(move => move.CapturePieceType))
                 {
-                    if (piece.IsWhite)
+                    board.MakeMove(move);
+                    score = -negamax(remainingDepth - 1, -beta, -alpha);
+                    board.UndoMove(move);
+                    if (score < alpha)
                     {
-                        white += pieceValues[(int)piece.PieceType];
-                    }
-                    else
-                    {
-                        black += pieceValues[(int)piece.PieceType];
+                        if (remainingDepth == 5)
+                            rootMove = move;
+                        alpha = score;
+                        if (score <= beta) break;
                     }
                 }
+
+                return alpha;
             }
-            int whosTurn = board.IsWhiteToMove ? 1 : -1;
-
-            return ((white - black) * whosTurn) / 100;
         }
-
-        bool IsPawnAttacking(Square targetSquare, ulong pawnAttackBoard)
-        {
-            //check if move is in
-            if (BitboardHelper.SquareIsSet(pawnAttackBoard, targetSquare))
-                return true;
-            return false;
-        }
-
-        ulong GeneratePawnAttackMap(Board board)
-        {
-            ulong pawnAttackBoard = 0;
-
-            foreach (Piece piece in board.GetPieceList(PieceType.Pawn, !board.IsWhiteToMove))
-                pawnAttackBoard = pawnAttackBoard | BitboardHelper.GetPawnAttacks(piece.Square, !board.IsWhiteToMove);
-
-            return pawnAttackBoard;
-        }
-
-        int GetPieceValue(PieceType pieceType)
-        {
-            return pieceValues[(int)pieceType];
-        }
-
-        //prioritize high value moves
-        //unsure how to implement this into the algorithm
-        List<Move> OrderMoves(Move[] moves, Board board)
-        {
-            List<Tuple<Move, int>> moveOrder = new List<Tuple<Move, int>>();
-
-            pawnAttackBitboard = GeneratePawnAttackMap(board);
-            //based on what category a move falls into it should be placed either at the front, middle, or back of the new array
-            foreach (Move move in moves)
-            {
-                int movePriority = 0;
-                PieceType movePieceType = move.MovePieceType;
-                PieceType capturePieceType = move.CapturePieceType;
-
-                //should capture opponents best pieces
-                if (capturePieceType != PieceType.None && movePieceType != PieceType.King)
-                {
-                    movePriority = 10 * GetPieceValue(capturePieceType) - GetPieceValue(movePieceType);
-                }
-
-                //promoting pawns is good
-                if (move.IsPromotion)
-                {
-                    movePriority += GetPieceValue(move.PromotionPieceType);
-                }
-
-                //moving into opponents pawns is bad
-                bool movingIntoPawnAttack = IsPawnAttacking(move.TargetSquare, pawnAttackBitboard);
-                if (movingIntoPawnAttack)
-                {
-                    movePriority -= GetPieceValue(movePieceType);
-                }
-
-                //place move into correct spot in moveOrder
-                Tuple<Move, int> moveTuple = new Tuple<Move, int>(move, movePriority);
-
-                bool inserted = false;
-                for (int i = 0; i < moveOrder.Count; i++)
-                {
-                    //if higher priority than another move insert it at that spot
-                    if (movePriority >= moveOrder[i].Item2)
-                    {
-                        moveOrder.Insert(i, moveTuple);
-                        inserted = true;
-                        break;
-                    }
-                }
-                //if not greater or equal to any of the values in moveOrder add to back
-                if (!inserted)
-                    moveOrder.Add(moveTuple);
-            }
-
-            //get the order but leave the int values
-            List<Move> orderedMoveList = new List<Move>();
-            foreach (Tuple<Move, int> move in moveOrder)
-            {
-                orderedMoveList.Add(move.Item1);
-            }
-
-            return orderedMoveList;
-        }
-
-        //public void ToggleBitboard(Board board)
-        //{
-        //    return;
-        //}
-
-        //public void GenerateBitboard(Board board)
-        //{
-        //    return;
-        //}
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -3,261 +3,68 @@ using System;
 using System.Collections.Generic;
 using System.Numerics;
 using System.Linq;
-using System.Diagnostics;
+using System.Data;
 
 public class MyBot : IChessBot
 {
-    // Piece values: null, pawn, knight, bishop, rook, queen, king
-    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
-    int negativeInfinity = -99999;
-    int positiveInfinity = 99999;
-
-    //tracking opposing pawn structure
-    ulong pawnAttackBitboard;
-
-    //tracking best move
-    Move bestMove = Move.NullMove;
-    int bestEval = -99999;
-
-    struct SearchResult
-    {
-        public int bestEvaluation;
-        public Move bestMoveThisSearch;
-    }
+    int[] pieceValues = { 0, 120, 300, 310, 500, 900, 10000 };
+    Move rootMove;
 
     public Move Think(Board board, Timer timer)
     {
-        //get the best move
-        Move moveToPlay = FindBestMove(board);
+        Negamax(5, 99999, -99999);
+        return rootMove;
 
-        //ensure move is legal
-        if (!board.GetLegalMoves().Contains(moveToPlay)) { throw new Exception("ERROR: Attempted Move: " + moveToPlay.StartSquare + moveToPlay.TargetSquare); }
-
-        pawnAttackBitboard = GeneratePawnAttackMap(board);
-        BitboardHelper.VisualizeBitboard(pawnAttackBitboard);
-
-        return moveToPlay;
-    }
-
-    Move FindBestMove(Board board)
-    {
-        return Search(board, 4, negativeInfinity, positiveInfinity).bestMoveThisSearch;
-    }
-
-    SearchResult Search(Board board, int depth, int alpha, int beta)
-    {
-        if (depth == 0)
-            return SearchCaptures(board, alpha, beta);
-
-        List<Move> moves = OrderMoves(board.GetLegalMoves(), board);
-        //either in checkmate or draw
-        if (moves.Count == 0)
+        //functions inside of Think so don't need to reference board again to save tokens
+        long Negamax(int remainingDepth, long alpha, long beta)
         {
-            if (board.IsInCheck())
-                return new SearchResult { bestEvaluation = negativeInfinity, bestMoveThisSearch = Move.NullMove };
-            return new SearchResult { bestEvaluation = 0, bestMoveThisSearch = Move.NullMove };
-        }
+            if (board.IsDraw())
+                return 0;
+            if (board.IsInCheckmate())
+                return 90000;
 
-        bestMove = Move.NullMove;
-        bestEval = negativeInfinity;
+            //more moves = more ability find tactics/take control of the board
+            long score = -board.GetLegalMoves().Count() + Evaluate();
 
-        foreach (Move move in moves)
-        {
-            board.MakeMove(move);
-            int evaluation = -Search(board, depth - 1, -beta, -alpha).bestEvaluation;
-            board.UndoMove(move);
-            
-            if (evaluation >= beta)
-                return new SearchResult { bestMoveThisSearch = Move.NullMove, bestEvaluation = beta };
-            
-            alpha = Math.Max(alpha, evaluation);
-            if (alpha >= bestEval)
+            //check alpha & beta
+            if (remainingDepth <= 0)
             {
-                bestEval = alpha;
-                bestMove = move;
+                if (score < alpha)
+                    alpha = score;
+                if (score <= beta)
+                    return beta;
             }
-        }
-        
-        return new SearchResult { bestMoveThisSearch = bestMove, bestEvaluation = bestEval };
-    }
 
-    SearchResult SearchCaptures(Board board, int alpha, int beta)
-    {
-        int evaluation = Evaluate(board);
-        if (evaluation >= beta)
-            return new SearchResult { bestEvaluation = beta, bestMoveThisSearch = Move.NullMove };
-        alpha = Math.Max(alpha, evaluation);
-
-        List<Move> captureMoves = OrderMoves(board.GetLegalMoves(true), board);
-        bestMove = Move.NullMove;
-        bestEval = negativeInfinity;
-
-        foreach (Move capMove in captureMoves)
-        {
-            board.MakeMove(capMove);
-            evaluation = -SearchCaptures(board, -beta, -alpha).bestEvaluation;
-            board.UndoMove(capMove);
-            
-            if (evaluation >= beta)
-                return new SearchResult { bestMoveThisSearch = Move.NullMove, bestEvaluation = beta };
-            alpha = Math.Max(alpha, evaluation);
-            if (alpha >= bestEval)
+            //for each move in an ordered set of legal moves (ordered based on the type of capture piece, so prioritizes captures first) (if depth is 0 then only do captures)
+            foreach (Move move in board.GetLegalMoves(remainingDepth <= 0).OrderByDescending(move => move.CapturePieceType))
             {
-                bestEval = alpha;
-                bestMove = capMove;
-            }
-        }
-
-        return new SearchResult { bestEvaluation = bestEval, bestMoveThisSearch = bestMove };
-    }
-
-    int Evaluate(Board board)
-    {
-        int white = 0;
-        int black = 0;
-        int numPiecesWhite = 0;
-        int numPiecesBlack = 0;
-
-        PieceList[] pieces = board.GetAllPieceLists();
-        foreach (PieceList pieceList in pieces)
-        {
-            foreach (Piece piece in pieceList)
-            {
-                
-                if (piece.IsWhite)
+                board.MakeMove(move);
+                score = -Negamax(remainingDepth - 1, -beta, -alpha);
+                board.UndoMove(move);
+                //check board state
+                if (score < alpha)
                 {
-                    numPiecesWhite++;
-                    white += pieceValues[(int)piece.PieceType];
-                }
-                else
-                {
-                    numPiecesBlack++;
-                    black += pieceValues[(int)piece.PieceType];
+                    //store root move (current depth value is 5)
+                    if (remainingDepth == 5)
+                        rootMove = move;
+                    alpha = score;
+                    if (score <= beta)
+                        break;
                 }
             }
+
+            return alpha;
         }
 
-        float endgameWeight = ( (board.IsWhiteToMove && numPiecesBlack <= 3) || (!board.IsWhiteToMove && numPiecesWhite <= 3) ) ? (float)1 / (float)(64) : 0;
-        int kingToCornerWeight = ForceKingToCorner(board.GetPieceList(PieceType.King, board.IsWhiteToMove)[0].Square.Index, board.GetPieceList(PieceType.King, !board.IsWhiteToMove)[0].Square.Index, endgameWeight);
-
-        int whosTurn = board.IsWhiteToMove ? 1 : -1;
-
-        return ((white - black) * whosTurn) / 100;
-        //return ((white - black + kingToCornerWeight) * whosTurn) / 100;
-    }
-
-    int ForceKingToCorner(int friendlyKingSquare, int opponentKingSquare, float endgameWeight)
-    {
-        int evaluation = 0;
-
-        //prioritize positions where the opposing king has been forced from the center
-        int opponentKingRank = opponentKingSquare >> 3;
-        int opponentKingFile = opponentKingSquare & 0b000111;
-
-        int oppKingDstToCenterRank = Math.Max(3 - opponentKingRank, opponentKingRank - 4);
-        int oppKingDstToCenterFile = Math.Max(3 - opponentKingFile, opponentKingFile - 4);
-        int oppKingDstFromCenter = oppKingDstToCenterFile + oppKingDstToCenterRank;
-        evaluation += oppKingDstFromCenter;
-
-        //incentivise moving king closer to opponent king
-        int friendlyKingRank = friendlyKingSquare >> 3;
-        int friendlyKingFile = friendlyKingSquare & 0b000111;
-
-        int dstBetweenKingsRank = Math.Abs(friendlyKingRank - opponentKingRank);
-        int dstBetweenKingsFile = Math.Abs(friendlyKingFile - opponentKingFile);
-        int dstBetweenKings = dstBetweenKingsRank + dstBetweenKingsFile;
-        evaluation += 14 - dstBetweenKings;
-
-        return (int)(evaluation * 10 * endgameWeight);
-    }
-
-    bool IsPawnAttacking(Square targetSquare)
-    {
-        //check if move is in
-        if (BitboardHelper.SquareIsSet(pawnAttackBitboard, targetSquare))
-            return true;
-        return false;
-    }
-
-    ulong GeneratePawnAttackMap(Board board)
-    {
-        ulong pawns = board.GetPieceBitboard(PieceType.Pawn, !board.IsWhiteToMove);
-        ulong pawnAttackBoard = 0b0;
-
-        for (int i = 0; i < 64; i++)
+        long Evaluate()
         {
-            //if value on bitboard at index of i is 1
-            if ( (pawns & ((ulong)1 << i) ) != 0)
-                pawnAttackBoard = pawnAttackBoard | BitboardHelper.GetPawnAttacks(new Square(i), !board.IsWhiteToMove);
+            long eval = 0;
+            foreach (PieceList pieces in board.GetAllPieceLists())
+            {
+                //count of pieces in list * value of that type of piece * color of that type of piece
+                eval += pieces.Count * (pieceValues[(int)pieces.TypeOfPieceInList]) * (pieces.IsWhitePieceList == board.IsWhiteToMove ? -1 : 1);
+            }
+            return eval;
         }
-
-        return pawnAttackBoard;
-    }
-
-    int GetPieceValue(PieceType pieceType)
-    {
-        return pieceValues[(int)pieceType];
-    }
-
-    //prioritize high value moves
-    //unsure how to implement this into the algorithm
-    List<Move> OrderMoves(Move[] moves, Board board)
-    {
-        List<Tuple<Move, int>> moveOrder = new List<Tuple<Move, int>>();
-
-        pawnAttackBitboard = GeneratePawnAttackMap(board);
-        //based on what category a move falls into it should be placed either at the front, middle, or back of the new array
-        foreach (Move move in moves)
-        {
-            int movePriority = 0;
-            PieceType movePieceType = move.MovePieceType;
-            PieceType capturePieceType = move.CapturePieceType;
-
-            //should capture opponents best pieces
-            if (capturePieceType != PieceType.None)
-            {
-                movePriority = 10 * GetPieceValue(capturePieceType) - GetPieceValue(movePieceType);
-            }
-
-            //promoting pawns is good
-            if (move.IsPromotion)
-            {
-                movePriority += GetPieceValue(move.PromotionPieceType);
-            }
-
-            //moving into opponents pawns is bad
-            bool movingIntoPawnAttack = IsPawnAttacking(move.TargetSquare);
-            if (movingIntoPawnAttack)
-            {
-                movePriority -= GetPieceValue(movePieceType);
-            }
-
-            //place move into correct spot in moveOrder
-            Tuple<Move, int> moveTuple = new Tuple<Move, int>(move, movePriority);
-
-            bool inserted = false;
-            for (int i = 0; i < moveOrder.Count; i++)
-            {
-                //if higher priority than another move insert it at that spot
-                if (movePriority >= moveOrder[i].Item2)
-                {
-                    moveOrder.Insert(i, moveTuple);
-                    inserted = true;
-                    break;
-                }
-            }
-            //if not greater or equal to any of the values in moveOrder add to back
-            if (!inserted)
-                moveOrder.Add(moveTuple);
-        }
-
-        //get the order but leave the int values
-        List<Move> orderedMoveList = new List<Move>();
-        foreach (Tuple<Move, int> move in moveOrder)
-        {
-            orderedMoveList.Add(move.Item1);
-        }
-
-        return orderedMoveList;
     }
 }
